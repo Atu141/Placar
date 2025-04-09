cf07f398b5677923e5d5f84c4a4e77be54274a8f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-09 10:51:47 -0300

    Adição das configirações da PlayerActivity e MainActivity

--- app/src/main/java/com/example/placar/PlayerActivity.kt
+++ app/src/main/java/com/example/placar/PlayerActivity.kt
@@ -1,20 +1,30 @@
 package com.example.placar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.placar.databinding.ActivityPlayerBinding
+import com.example.placar.databinding.ActivitySplashBinding
 
 class PlayerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_player)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btStart.setOnClickListener{
+            val nextScreem = Intent(this, MainActivity::class.java)
+            nextScreem.putExtra("PLAYER1", binding.etPlayer1.text.toString())
+            nextScreem.putExtra("PLAYER2", binding.etPlayer2.text.toString())
+            startActivity(nextScreem)
         }
+
     }
 }
